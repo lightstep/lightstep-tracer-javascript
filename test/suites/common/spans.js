e453be8183e5fe7cd6bd523bb6e64beee73a2969
bcronin <bcronin@resonancelabs.com>
2016-03-25 14:58:44 -0700

    Update inject/join semantics

--- test/suites/common/spans.js
+++ test/suites/common/spans.js
@@ -37,34 +37,33 @@ it("should emit a 'span_added' event when each span ends", function() {
     expect(count).to.equal(2);
 });
 
-it("should handle inject / join to split text carriers correctly" , function() {
+it("should handle inject / join to text map carriers correctly" , function() {
     var span = Tracer.startSpan('my_span');
     span.setBaggageItem('footwear', 'sandals');
     span.setBaggageItem('creditcard', 'visa');
 
-    var carrier = new Tracer.SplitTextCarrier();
-    Tracer.inject(span, Tracer.FORMAT_SPLIT_TEXT, carrier);
+    var carrier = {};
+    Tracer.inject(span, Tracer.FORMAT_TEXT_MAP, carrier);
+    expect(carrier['ot-tracer-traceid']).to.equal(span.imp().traceGUID());
+    expect(carrier['ot-tracer-spanid']).to.equal(span.imp().guid());
+    expect(carrier['ot-baggage-footwear']).to.equal('sandals');
+    expect(carrier['ot-baggage-creditcard']).to.equal('visa');
 
-    expect(carrier.tracerState['ot-tracer-traceid']).to.equal(span.imp().traceGUID());
-    expect(carrier.tracerState['ot-tracer-spanid']).to.equal(span.imp().guid());
-    expect(carrier.baggage['ot-baggage-footwear']).to.equal('sandals');
-    expect(carrier.baggage['ot-baggage-creditcard']).to.equal('visa');
-
-    var naps = Tracer.join('naps_ym', Tracer.FORMAT_SPLIT_TEXT, carrier);
+    var naps = Tracer.join('naps_ym', Tracer.FORMAT_TEXT_MAP, carrier);
     expect(naps.imp().parentGUID()).to.equal(span.imp().guid());
     expect(naps.getBaggageItem('footwear')).to.equal('sandals');
     expect(naps.getBaggageItem('creditcard')).to.equal('visa');
 });
 
-it("should handle inject / join to binary carriers correctly" , function() {
+it("should handle inject / join to carriers correctly" , function() {
     var span = Tracer.startSpan('my_span');
     span.setBaggageItem('footwear', 'sandals');
     span.setBaggageItem('creditcard', 'visa');
 
     var carrier = new Tracer.BinaryCarrier();
-    Tracer.inject(span, Tracer.FORMAT_SPLIT_BINARY, carrier);
+    Tracer.inject(span, Tracer.FORMAT_BINARY, carrier);
 
-    var naps = Tracer.join('naps_ym', Tracer.FORMAT_SPLIT_BINARY, carrier);
+    var naps = Tracer.join('naps_ym', Tracer.FORMAT_BINARY, carrier);
     expect(naps.imp().parentGUID()).to.equal(span.imp().guid());
     expect(naps.getBaggageItem('footwear')).to.equal('sandals');
     expect(naps.getBaggageItem('creditcard')).to.equal('visa');
