c4bbfde59fe7cd42c521835df59bb1eff115447b
bcronin <bcronin@lightstep.com>
2016-05-11 16:07:28 -0700

    Revert modified example

--- examples/node-trivial/index.js
+++ examples/node-trivial/index.js
@@ -6,22 +6,15 @@ var LightStep = require('../../dist/lightstep-tracer-node-debug');
 Tracer.initGlobalTracer(LightStep.tracer({
     access_token   : '{your_access_token}',
     component_name : 'lightstep-tracer/examples/node-trivial',
-    collector_host : 'localhost',
-    collector_port : 9998,
-    collector_encryption : 'none',
-    //disable_reporting_loop : true,
 }));
 
-var parent = Tracer.startSpan('parent');
-for (var i = 0; i < 20000; i++) {
-    var child = Tracer.startSpan('child', { parent: parent });
-    child.logEvent('log_event', largePayload);
-    child.finish();
-}
-parent.finish();
+var span = Tracer.startSpan('trivial_span');
+setTimeout(function() {
+    span.logEvent('log_event');
+}, 100);
+setTimeout(function() {
+    span.finish();
+}, 200);
 
-var url = parent.imp().generateTraceURL();
-console.log();
-console.log(url);
-console.log();
-console.log(Tracer.imp().stats());
+var url = span.imp().generateTraceURL();
+console.log('URL: ' + url);
