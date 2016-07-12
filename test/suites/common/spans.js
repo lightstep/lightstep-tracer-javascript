3824ffdfb714cf34fe11bdca69d687ee81e4bb76
Ben Sigelman <bhs@resonancelabs.com>
2016-07-11 21:51:31 -0700

    Adjust to OT PR changes

--- test/suites/common/spans.js
+++ test/suites/common/spans.js
@@ -4,7 +4,7 @@ it("should capture parent span guids", function () {
 
     var parent = Tracer.startSpan("A");
     var child = Tracer.startSpan("B", {
-        reference : Tracer.childOf(parent.context()),
+        childOf : Tracer.childOf(parent.context()),
     });
     child.finish();
     parent.finish();
