cc90fb93e041538ea95754048ad28d0f3d0b055d
bcronin <bcronin@lightstep.com>
2016-08-01 16:49:15 -0700

    Amend code comment

--- src/_each.js
+++ src/_each.js
@@ -1,4 +1,4 @@
-// Underscore.js-like wrapper to iterate for keys. Note that even for completely
+// Underscore.js-like wrapper to iterate object key-values. Note that even for completely
 // internal objects, packages may modify default Object prototypes and properties
 // (e.g. Ember.js) so it's almost never safe to assume a particular object can
 // iterated with for-in.
