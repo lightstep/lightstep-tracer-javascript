b704c1ef86f2eeda7d101b16ba851ee3a61ac831
bcronin <bcronin@resonancelabs.com>
2016-08-04 12:22:27 -0700

    Defect/payload not valid string (#69)
    * Protect against undefined, null strings
    
    * Fix case where payload is gzipped binary data

--- src/imp/platform/node/transport_httpjson.js
+++ src/imp/platform/node/transport_httpjson.js
@@ -6,7 +6,7 @@ const kMaxDetailedErrorFrequencyMs = 30000;
 const kMaxStringLength = 2048;
 
 function truncatedString(s) {
-    if (s.length <= kMaxStringLength) {
+    if (!s || s.length <= kMaxStringLength) {
         return s;
     }
     let half = Math.floor(kMaxStringLength / 2);
@@ -22,7 +22,9 @@ function encodeAndTruncate(obj) {
 }
 
 function errorFromResponse(res, buffer) {
-    buffer = truncatedString(`${buffer}`.replace(/\s+$/, ''));
+    if (buffer && buffer.length) {
+        buffer = truncatedString(`${buffer}`.replace(/\s+$/, ''));
+    }
     return new Error(`status code=${res.statusCode}, message='${res.statusMessage}', body='${buffer}'`);
 }
 
@@ -126,7 +128,7 @@ export default class TransportHTTPJSON {
                     this._error('HTTP request error', {
                         error  : err,
                         extra  : extraErrorData,
-                        report : truncatedString(payload),
+                        report : encodeAndTruncate(reportRequest),
                     });
                 });
                 done(err, null);
