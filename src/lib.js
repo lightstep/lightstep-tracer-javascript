83ee4ae49be689a4fe198f46ea5b769847ff9690
Conrad Buck <conrad@plangrid.com>
2020-02-24 12:37:17 -0800

    export SpanContext
    Fixes #229

--- src/lib.js
+++ src/lib.js
@@ -2,6 +2,7 @@ import Tracer from './imp/tracer_imp';
 import LightStepPropagator from './imp/propagator_ls';
 import B3Propagator from './imp/propagator_b3';
 import DDPropagator from './imp/propagator_dd';
+import SpanContext from './imp/span_context_imp';
 import { Platform } from './platform_abstraction_layer';
 
 const library = {
@@ -9,6 +10,7 @@ const library = {
     LightStepPropagator : LightStepPropagator,
     B3Propagator        : B3Propagator,
     DDPropagator        : DDPropagator,
+    SpanContext         : SpanContext,
 };
 
 Platform.initLibrary(library);
