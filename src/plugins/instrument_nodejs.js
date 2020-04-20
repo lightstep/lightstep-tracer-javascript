54082659218b11850aa03a5cc7d32210b577cd1b
Jose <joses@spotify.com>
2020-04-20 16:21:54 -0400

    change in the options creation

--- src/plugins/instrument_nodejs.js
+++ src/plugins/instrument_nodejs.js
@@ -63,8 +63,8 @@ class InstrumentNodejs {
 
     addOptions(tracerImp) {
         tracerImp.addOption('nodejs_instrumentation', { type : 'bool', defaultValue : false });
-        tracerImp.addOption('nodejs_inclusion_patterns', { type : 'array', defaultValue : [/.*/] });
-        tracerImp.addOption('nodejs_exclusion_patterns', { type : 'array', defaultValue : [] });
+        tracerImp.addOption('nodejs_url_inclusion_patterns', { type : 'array', defaultValue : [/.*/] });
+        tracerImp.addOption('nodejs_url_exclusion_patterns', { type : 'array', defaultValue : [] });
         tracerImp.addOption('include_cookies', { type : 'bool', defaultValue : true });
     }
 
@@ -283,10 +283,10 @@ class InstrumentNodejs {
         }
 
         let include = false;
-        if (opts.nodejs_inclusion_patterns.some((inc) => inc.test(url))) {
+        if (opts.nodejs_url_inclusion_patterns.some((inc) => inc.test(url))) {
             include = true;
         }
-        if (opts.nodejs_exclusion_patterns.some((ex) => ex.test(url))) {
+        if (opts.nodejs_url_exclusion_patterns.some((ex) => ex.test(url))) {
             include = false;
         }
         return include;
