58c880848eae09b4b0b64195c56af146cd983053
ODEit <jose.sabal@yahoo.com>
2021-06-15 19:52:03 -0400

    add missing span context OpenTracing methods

--- test/suites/common/span_context.js
+++ test/suites/common/span_context.js
@@ -18,4 +18,24 @@ describe("SpanContext", function() {
             expect(spanContext.getBaggageItem).to.be.a("function");
         });
     });
+
+    describe("SpanContext#toTraceId", function() {
+        it('is a method', function() {
+            var span = Tracer.startSpan('test');
+            var spanContext = span.context();
+            span.finish();
+
+            expect(spanContext.toSpanId).to.be.a("function");
+        });
+    });
+
+    describe("SpanContext#toSpanId", function() {
+        it('is a method', function() {
+            var span = Tracer.startSpan('test');
+            var spanContext = span.context();
+            span.finish();
+
+            expect(spanContext.toTraceId).to.be.a("function");
+        });
+    });
 });
